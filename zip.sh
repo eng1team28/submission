4d6c3c0752c9d8e69ed2c5156bc153a623d7728e
jmcb <joelsgp@protonmail.com>
2024-02-20 12:58:22 +0000

    Work on script

--- zip.sh
+++ zip.sh
@@ -1,14 +1,28 @@
 #!/bin/bash
 set -eux
 
-git submodule update
+# update submodules
+git submodule foreach git pull
 
+# create folder for submission
 zipdir='exam'
-
+rm -rf "${zipdir}"
 mkdir -p "${zipdir}"
-cp eng1team28.github.io/pdf/* "${zipdir}/"
-cp zip.sh "${zipdir}"
 
+# copy url file
+cp url1.txt "${zipdir}"
+
+# copy pdfs
+cp eng1team28.github.io/pdf/* "${zipdir}/"
+
+# copy source code
+# checked-in files only using git archive
+mkdir -p "${zipdir}/game/"
 git -C game/ archive --format='tar' 'HEAD' | bsdtar --extract --directory "${zipdir}/game"
 
-bsdtar --auto-compress --create --file 'exam.zip' "${zipdir}"
+# get executable jar
+# todo enable
+# wget --directory-prefix="${zipdir}" 'https://github.com/eng1team28/game/releases/download/latest/HeslingtonHustle.jar'
+
+# create submission zip
+bsdtar --auto-compress --create --file 'exam.zip' --directory "${zipdir}" ./
