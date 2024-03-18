79f10a76a85f49b66c92d602622b8c93a793f6a2
jmcb <joelsgp@protonmail.com>
2024-03-18 16:16:31 +0000

    Enable jar download, zip latest version

--- zip.sh
+++ zip.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -eux
 
+jar_name='HeslingtonHustle.jar'
 zipdir='exam'
 zip_file="${1:-exam.zip}"
 
@@ -8,11 +9,16 @@ zip_file="${1:-exam.zip}"
 rm -rf "${zipdir}"
 mkdir -p "${zipdir}"
 
-# update submodules
-git submodule foreach git pull
-
 # copy url file
 cp url1.txt "${zipdir}"
+
+# get executable jar
+wget \
+    --directory-prefix="${zipdir}" \
+    "https://github.com/eng1team28/game/releases/latest/download/${jar_name}"
+
+# update submodules
+git submodule foreach git pull
 
 # copy pdfs
 cp eng1team28.github.io/pdf/* "${zipdir}/"
@@ -21,10 +27,6 @@ cp eng1team28.github.io/pdf/* "${zipdir}/"
 # checked-in files only using git archive
 mkdir -p "${zipdir}/game/"
 git -C game/ archive --format='tar' 'HEAD' | bsdtar --extract --directory "${zipdir}/game"
-
-# get executable jar
-# todo enable
-# wget --directory-prefix="${zipdir}" 'https://github.com/eng1team28/game/releases/download/latest/HeslingtonHustle.jar'
 
 # create submission zip
 bsdtar --auto-compress --create --file "${zip_file}" --directory "${zipdir}" ./
